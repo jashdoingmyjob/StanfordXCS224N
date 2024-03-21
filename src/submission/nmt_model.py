43ef21973fb8eb0c5f9b5daa6844fc397513c20b
Jash Vora <jashvora@jashs-mbp.lan>
2024-03-21 12:45:05 -0700

    changed indent

--- src/submission/nmt_model.py
+++ src/submission/nmt_model.py
@@ -349,11 +349,11 @@ class NMT(nn.Module):
         ###     Tanh:
         ###         https://pytorch.org/docs/stable/generated/torch.tanh.html#torch-tanh
         ### START CODE HERE (~6 Lines)
-            alpha_t = F.softmax(e_t, dim=1)
-            a_t = torch.bmm(alpha_t.unsqueeze(1), enc_hiddens).squeeze(1)
-            U_t = torch.cat([dec_hidden, a_t], dim=1)
-            V_t = self.combined_output_projection(U_t)
-            O_t = self.dropout(torch.tanh(V_t))
+        alpha_t = F.softmax(e_t, dim=1)
+        a_t = torch.bmm(alpha_t.unsqueeze(1), enc_hiddens).squeeze(1)
+        U_t = torch.cat([dec_hidden, a_t], dim=1)
+        V_t = self.combined_output_projection(U_t)
+        O_t = self.dropout(torch.tanh(V_t))
         ### END CODE HERE
 
         combined_output = O_t
